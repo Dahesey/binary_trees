f60f4ef7dd78917eea19b9251a7c8f98810e1565
Dahesey <dahesey@gmail.com>
2024-01-04 14:59:13 +0000

    A functiont o check the height of a tree

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+#include <stddef.h>
+
+/**
+ * binary_tree_height - A function to check the height of a tree
+ *
+ * @tree: The tree to be checked
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+
+	return (MAX(left_height, right_height) + 1);
+}

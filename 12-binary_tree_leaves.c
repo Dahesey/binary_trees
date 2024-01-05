56774145459d881b4cbff53127f33922ad4713e4
Dahesey <dahesey@gmail.com>
2024-01-05 12:40:38 +0000

    Fixed mixed declaration

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,14 @@
+#include "binary_tree.h"
+#include <stddef.h>
+
+/**
+ * binary_tree_leaves - A function to count the leaves on a binary tree
+ *
+ * @tree: The tree 
+ *
+ * Return: The count
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,10 +11,13 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height;
+	size_t right_height;
+
 	if (tree == NULL)
 		return (0);
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	return (MAX(left_height, right_height) + 1);
 }

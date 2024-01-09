7b3d2471893e21e7c557e3a2e18c89f57c233be0
Dahesey <dahesey@gmail.com>
2024-01-09 10:18:47 +0000

    Defined a new function

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -11,4 +11,6 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
 
--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 #include <stdlib.h>
+/**
+ * is_a_root - A function to find the root of a tree
+ *
+ * @node: The node
+ */
+#define is_a_root(node) (!(node)->parent ? 1 : 0)
+
 
 /**
  * binary_tree_is_root - A function to check if a node is a root
@@ -11,10 +18,5 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node == NULL)
-		return (0);
-	if (node->left != NULL && node->right != NULL)
-		return (1);
-	else
-		return (0);
+	return (node == NULL ? 0 : is_a_root(node));
 }
